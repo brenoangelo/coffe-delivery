89af0c12e273216962a284dc32f937d5307b159c
Breno Angelo <brenoangeloal@gmail.com>
2023-02-28 21:56:35 -0300

    create initial structure

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,21 @@
+export const defaultTheme = {
+  white: '#ffffff',
+
+  'gray-900': '#272221',
+  'gray-800': '#403937',
+  'gray-700': '#574f4d',
+  'gray-600': '#8d8686',
+  'gray-500': '#d7d5d5',
+  'gray-400': '#e6e5e5',
+  'gray-300': '#ededed',
+  'gray-200': '#f3f2f2',
+  'gray-100': '#fafafa',
+
+  'yellow-700': '#c47f17',
+  'yellow-300': '#dbac2c',
+  'yellow-100': '#f1e9c9',
+
+  'purple-700': '#4b2995',
+  'purple-300': '#8047f8',
+  'purple-100': '#ebe5f9',
+};
